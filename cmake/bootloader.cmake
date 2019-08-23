58a727ae3ec2382e77892365295be4480d85e1ef
Taras Mykhalevych <taras.mykhalevych@ring.com>
2019-08-23 15:36:55 +0300

    Add broadcom bl config files

--- cmake/bootloader.cmake
+++ cmake/bootloader.cmake
@@ -1,8 +1,9 @@
 include(cmake/board.cmake)
 
-set(BROADCOM_PRECOMPILED ${PROJECT_SOURCE_DIR}/boot/broadcom)
-set(UBOOT_SRC_DIR ${PROJECT_SOURCE_DIR}/boot/u-boot)
-set(BOOT_TOOLS_DIR ${PROJECT_SOURCE_DIR}/boot/tools)
+set(BOOT_DIR ${PROJECT_SOURCE_DIR}/boot)
+set(BROADCOM_PRECOMPILED ${BOOT_DIR}/broadcom)
+set(UBOOT_SRC_DIR ${BOOT_DIR}/u-boot)
+set(BOOT_TOOLS_DIR ${BOOT_DIR}/tools)
 set(UBOOT_BUILD_DIR ${CMAKE_BINARY_DIR}/u-boot)
 set(UBOOT_CROSSCOMPILE_PARAMS CROSS_COMPILE=$ENV{QB_BUILD_TOOL_NAME}- O=${UBOOT_BUILD_DIR})
 set(UBOOT_MAKE_PARAMS) # Currently there is nothing here
@@ -17,6 +18,8 @@ add_custom_target( bl-build # bl for bootloader
     # TODO: Generate ${FIRMWARE_BOOT_ROOT}/u-boot.img
     COMMAND touch ${FIRMWARE_BOOT_ROOT}/uEnv.txt
     COMMAND sh ${BOOT_TOOLS_DIR}/uEnv.txt.filler ${FIRMWARE_BOOT_ROOT}/uEnv.txt
+    COMMAND cp -u ${BOOT_DIR}/config.txt ${FIRMWARE_BOOT_ROOT}/config.txt
+    COMMAND cp -u ${BOOT_DIR}/cmdline.txt ${FIRMWARE_BOOT_ROOT}/cmdline.txt
     COMMAND cp -u ${BROADCOM_PRECOMPILED}/* ${FIRMWARE_BOOT_ROOT}/
     # TODO: Create and copy config.txt and other config files for RPi bootloaders
     WORKING_DIRECTORY ${UBOOT_SRC_DIR}
